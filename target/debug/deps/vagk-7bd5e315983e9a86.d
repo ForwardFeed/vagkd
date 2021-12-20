dde18393aa7ebc9f9d8dd0f25ffefffc946ba058
WhereTheFuckIsMySerotonin <76596109+WhereTheFuckIsMySerotonin@users.noreply.github.com>
2021-12-21 00:36:03 +0100

    lezzfuckngo

---
+++ target/debug/deps/vagk-7bd5e315983e9a86.d
@@ -0,0 +1,5 @@
+/home/notalinux/programation/Rust/vagk/target/debug/deps/vagk-7bd5e315983e9a86: src/main.rs
+
+/home/notalinux/programation/Rust/vagk/target/debug/deps/vagk-7bd5e315983e9a86.d: src/main.rs
+
+src/main.rs:
---
+++ target/debug/vagk.d
@@ -0,0 +1,1 @@
+/home/notalinux/programation/Rust/vagk/target/debug/vagk: /home/notalinux/programation/Rust/vagk/src/main.rs
